a7cd881556bddc7821267726e6b80f0451cd2c82
Qluqlu3 <plksmcgof@gmail.com>
2026-06-13 22:47:33 +0900

    feat: Chakra UI を導入・Provider を設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Providers } from './providers';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: '日本の特定外来生物',
@@ -24,11 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="ja"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ja">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
